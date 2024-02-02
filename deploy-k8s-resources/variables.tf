48bf6c3041ec08d7a7d1c5907666f72dda84b5f2
Brent Yarger <brent.yarger@konghq.com>
2024-02-02 14:35:45 -0800

    Add additional vars to define git repo, image semver

--- deploy-k8s-resources/variables.tf
+++ deploy-k8s-resources/variables.tf
@@ -8,8 +8,20 @@ variable "kong_enterprise" {
   default = false
 }
 
+variable "kong_repository" {
+  description = "Kong image repository"
+  type =  string
+  default = "kong/kong-gateway"
+}
+
 variable "kong_version" {
   description = "Kong version to deploy"
   type =  string
   default = "3.5"
+}
+
+variable "kong_effective_semver" {
+  description = "Semantic version, required if using a kong_version that does not look like a semver, e.g. 'nightly'"
+  type =  string
+  default = null
 }
